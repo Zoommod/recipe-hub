f2e8ae5e25e3315ae164737db4f3bbff9a132ddd
Zoommod <vitor.sacramento09@gmail.com>
2024-08-21 22:57:56 -0300

    MOD: assigned unique names to each URL

--- utils/recipe/test.py
+++ utils/recipe/test.py
@@ -1,5 +1,6 @@
 from random import randint
 from faker import Faker
+import random
 
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
@@ -8,6 +9,7 @@ fake = Faker('pt_BR')
 
 def make_recipe():
     return {
+        'id': random.randint(1, 1000000),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
