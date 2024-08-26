cdf0fd0e4544f4bdca16a9e89bb61c89fd47b3ea
Zoommod <vitor.sacramento09@gmail.com>
2024-08-26 01:57:13 -0300

    FIX: hidden unpublished recipes from homepage and category pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from utils.recipe.test import make_recipe
 from .models import Recipe, Category
 
 def home (request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes
     })
 
 def recipe (request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
 def category(request, category_id):
-    category = get_object_or_404(Category, id=category_id)
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'category': category,
     })
