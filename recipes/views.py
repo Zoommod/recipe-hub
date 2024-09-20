2bea90148a127d06afe433c51d7bd4a165b707c6
Zoommod <01202021663>
2024-09-20 11:07:08 -0300

    ADD: started preparing unit tests with Django Pytest and Unittest

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from utils.recipe.test import make_recipe
 from .models import Recipe, Category
 
